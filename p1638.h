4e150cfdc33e26b674d7c1648068856248e45b65
1 <a1@1s-Mac-Pro.local>
2021-07-11 09:58:09 +0800

    a dp solved

---
+++ p1638.h
@@ -0,0 +1,21 @@
+//
+// Created by 1 on 2021/7/11.
+//
+
+#ifndef LEETCODE_P1638_H
+#define LEETCODE_P1638_H
+
+#endif //LEETCODE_P1638_H
+
+
+#include <string>
+#include <vector>
+
+
+using namespace std;
+
+class Solution_p1638
+{
+public:
+    int countSubstrings(string s, string t);
+};
